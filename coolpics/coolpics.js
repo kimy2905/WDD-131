0e6cfc88ade154a940df3159f724dd720e165e08
Kimberly Juarez <jua22004@byui.edu>
2024-10-16 10:08:11 -0600

    coolpics

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -11,7 +11,7 @@ function viewerTemplate(pic, alt) {
   
     if (clickedElement.tagName === "IMG") {
       const srcParts = clickedElement.src.split("$");
-      const newSrc = `${srcParts[0]}-full.jpeg`;
+      const newSrc = `${srcParts[0]}$full.jpeg`;
       const modalHTML = viewerTemplate(newSrc, clickedElement.alt);
       document.body.insertAdjacentHTML("afterbegin", modalHTML);
   
